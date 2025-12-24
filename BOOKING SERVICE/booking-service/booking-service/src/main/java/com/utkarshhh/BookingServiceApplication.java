0f0c5378350d5e37e66b6cac5d4cb2f1c65ee9ca
Utkarsh-Shaning <utkarsh1605shaning@gmail.com>
2025-12-24 11:50:10 +0530

    Eureka Server and Client to all services added

--- BOOKING SERVICE/booking-service/booking-service/src/main/java/com/utkarshhh/BookingServiceApplication.java
+++ BOOKING SERVICE/booking-service/booking-service/src/main/java/com/utkarshhh/BookingServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class BookingServiceApplication {
 
 	public static void main(String[] args) {
